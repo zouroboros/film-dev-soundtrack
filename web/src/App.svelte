1a65d26a3360ee41eef00b9a3e02673b199abdd9
Zouroboros <zouroboros@zoho.eu>
2022-09-11 15:01:46 +0200

    A list of instructions is shown once the play button is pressed.

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,12 +1,14 @@
 <script>
 	import audioBuffer2Wav from './wav.js';
-	import { playInstructions, downloadInstructions } from './instructions.js';
+	import { playInstructions, downloadInstructions } from './audioInstructions.js';
+	import { textInstructions } from './textInstructions';
 
 	let developDurationMinutes = 10;
 	let developDurationSeconds = 30;
 	let agitationContinously = 30;
 	let agitationInterval = 1;
 	let agitationLength = 10;
+	let currentTextInstructions = [];
 
 	const readOptions = function() {
 		return {
@@ -18,7 +20,9 @@
 
 	const play = function() {
 		playInstructions(readOptions());
+		updateInstructions();
 	};
+	
 	const download = function() {
 		downloadInstructions(readOptions()).then(buffer => {
 			const downloadLink = document.createElement('a');
@@ -27,6 +31,12 @@
 			downloadLink.dispatchEvent(new MouseEvent('click'));
 		})
 	};
+
+	const updateInstructions = function() {
+		currentTextInstructions = textInstructions(readOptions());
+	}
+
+	const formateMinutesAndSeconds = (seconds) => new Intl.DateTimeFormat(navigator.language, {minute: '2-digit', second: '2-digit'}).format(new Date(seconds * 1000));
 </script>
 
 <main>
@@ -39,10 +49,19 @@
 	</div>
 	<button on:click="{play}">Play</button>
 	<button on:click="{download}">Download</button>
+	<ol class="instructions">
+		{#each currentTextInstructions as instruction}
+			<li><span>{formateMinutesAndSeconds(instruction.time)}</span> {instruction.instruction}</li>			
+		{/each}
+	</ol>
 </main>
 
 <style>
 input {
 	width: 4em;
+}
+
+.instructions li span:first-child {
+	font-family: monospace, monospace;
 }
 </style>
---
+++ web/src/textInstructions.js
@@ -0,0 +1,17 @@
+export const textInstructions = function (options) {
+    const instructions = []
+
+    instructions.push({instruction: 'Pour in the developer', time: 0})
+    instructions.push({instruction: `Agitate continously for ${options.agitationContinously} seconds`, time: 0})
+    
+    const nAgitations = Math.floor(options.developDuration / options.agitationInterval / 60)
+
+    for(let agitation = 1; agitation <= nAgitations; agitation++) {
+        instructions.push({instruction: 'Start agitating.', time: agitation * options.agitationInterval * 60})
+        instructions.push({instruction: 'Stop agitating.', time: agitation * 60 + options.agitationLength})
+    }
+
+    instructions.push({instruction: 'Pour out the developer.', time: options.developDuration})
+
+    return instructions
+}
